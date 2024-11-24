b7b3ce97803f48a7e72ef81ced85640f5c890f27
roshan2007git <roshan.pramod.2007@gmail.com>
2024-11-24 13:47:36 +0530

    about page sorted

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,47 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='text-electricBlue lg:text-xl lg:px-44 text-center flex flex-col lg:gap-10 mb-52 gap-6 text-base px-7 md:px-16 md:gap-8 md:text-lg'>
+      <span className='font-bold lg:text-6xl text-4xl md:text-5xl'>About Us</span>
+      <p>
+        St Joseph’s Boys’ High School has been acclaimed for going
+        beyond the achievement of academic excellence since
+        1858. Our education is aimed at creating mature, capable
+        individuals, who can rise to the challenges of the changing
+        world. The Jesuit philosophy of ‘Fide Et Labore’ is embodied
+        in every aspect of our fests.
+      </p>
+      <p>
+        We are elated to announce the twelfth edition of our
+        trailblazing and exciting Cultural and Carnival Festival,
+        Phenomenon - One Space, Limitless Experiences.
+      </p>
+      <p>
+        Phenomenon, the vibrant fest organized by St. Joseph’s
+        Boys’ High School, is a celebration like no other! With an
+        incredible lineup of events spanning science, commerce,
+        music, literature, dance, and sports, there’s truly something
+        for everyone.
+      </p>
+      <p>
+        Phenomenon opens the gates to the world of creativity and
+        imagination. It showcases extraordinary talents and gives
+        the students a dynamic platform where culture meets
+        creativity, offering a rich tapestry of performances. At its
+        core, Phenomenon is about community and connection. It
+        allows participants and audiences to experience the beauty
+        of cultural diversity, fostering understanding and
+        appreciation for traditions both familiar and new. It’s a
+        space where passion, talent, and tradition converge, leaving
+        everyone inspired and enriched.
+      </p>
+      <p>
+        So, what are you waiting for? The spotlight awaits you!
+      </p>
+      <p>
+        If you are looking to make memories that last a lifetimePhenomenon 2024 is the place to be!
+      </p>
+    </div>
   )
 }
 
