818e17833de2094955dc0e57ca0cae7232323681
roshan2007git <roshan.pramod.2007@gmail.com>
2024-11-26 18:39:35 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -16,9 +16,11 @@ export default function NotFound() {
         transition={{
           duration:1
         }}
-      className="flex flex-col items-center justify-center h-screen text-[#0BB3BA]">
-        <h1 className="lg:text-4xl md:text-3xl text-2xl font-bold mb-4">404 - Page Not Found</h1>
-        <p className="mb-8 lg:text-base text-sm">Oops! The page you're looking for doesn't exist.</p>
+      className="flex flex-col items-center justify-center h-[100vh] text-[#0BB3BA]">
+        <div className="flex flex-col items-center lg:-translate-y-16 md:-translate-y-20 -translate-y-28">
+          <h1 className="lg:text-4xl md:text-3xl text-2xl font-bold mb-4">404 - Page Not Found</h1>
+          <p className="mb-8 lg:text-base text-sm">Oops! The page you're looking for doesn't exist.</p>
+        </div>
       </motion.div>
     </AnimatePresence>
   );
