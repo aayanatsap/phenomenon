589e7b01fb01e6298e6a11c95084ed508d7140bf
roshan2007git <roshan.pramod.2007@gmail.com>
2024-11-28 19:49:52 +0530

    Update page.tsx

--- app/artist/page.tsx
+++ app/artist/page.tsx
@@ -1,9 +1,28 @@
 "use client"
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Artist() {
   return (
-    <div>artist</div>
+    <AnimatePresence>
+      <motion.div
+        initial={{
+          opacity : 0
+        }}
+        animate={{
+          opacity: 1
+        }}
+        transition={{
+          duration:1
+        }}
+      className="flex flex-col items-center justify-center h-[100vh] text-[#0BB3BA]">
+        <div className="flex flex-col items-center lg:-translate-y-16 md:-translate-y-20 -translate-y-28">
+          <h1 className="lg:text-4xl md:text-3xl text-2xl font-bold mb-4">COMING SOON</h1>
+          <p className="mb-8 lg:text-base text-sm">The Artist for Phenomenon 2024 will be revelaed very soon!</p>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
