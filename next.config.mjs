15525647c762577b8a78f17b4e9acb085892fe9f
Aayan Bothra <110114427+aayanbothra@users.noreply.github.com>
2024-12-01 22:46:16 +0530

    Export preparation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-
-
-export default nextConfig;
+const nextConfig = {
+    //when testing through yarn dev, comment the whole thing till webpack
+    output: "export",
+    images: { unoptimized: true },
+    trailingSlash: true,
+    basePath: process.env.NODE_ENV === "production" ? "/phenomenon" : "",
+  
+    // // //uncomment this when not testing export
+    // assetPrefix:
+    //   process.env.NODE_ENV === "production"
+    //     ? "http://127.0.0.1:5500/sjbhsmun/"
+    //     : "http://127.0.0.1:5500/",
+  
+    // //uncomment this when exporting file for integro
+    assetPrefix:
+      process.env.NODE_ENV === "production"
+        ? "https://sjbhs.edu.in/phenomenon"
+        : "https://sjbhs.edu.in/",
+  
+    distDir: "export/phenomenon",
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: process.env.NEXT_PUBLIC_DISABLE_LINT === 'true',
+    },
+  };
+  
+  export default nextConfig;
