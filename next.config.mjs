e3bacc811a4ebe15f5c09a0ce15dac280fe0e9a1
roshan2007git <roshan.pramod.2007@gmail.com>
2024-12-01 23:40:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //when testing through yarn dev, comment the whole thing till webpack
-    output: "export",
-    images: { unoptimized: true },
-    trailingSlash: true,
-    basePath: process.env.NODE_ENV === "production" ? "/phenomenon" : "",
+    // output: "export",
+    // images: { unoptimized: true },
+    // trailingSlash: true,
+    // basePath: process.env.NODE_ENV === "production" ? "/phenomenon" : "",
   
-    // // //uncomment this when not testing export
+    // // // //uncomment this when not testing export
+    // // assetPrefix:
+    // //   process.env.NODE_ENV === "production"
+    // //     ? "http://127.0.0.1:5500/sjbhsmun/"
+    // //     : "http://127.0.0.1:5500/",
+  
+    // // //uncomment this when exporting file for integro
     // assetPrefix:
     //   process.env.NODE_ENV === "production"
-    //     ? "http://127.0.0.1:5500/sjbhsmun/"
-    //     : "http://127.0.0.1:5500/",
+    //     ? "https://sjbhs.edu.in/phenomenon"
+    //     : "https://sjbhs.edu.in/",
   
-    // //uncomment this when exporting file for integro
-    assetPrefix:
-      process.env.NODE_ENV === "production"
-        ? "https://sjbhs.edu.in/phenomenon"
-        : "https://sjbhs.edu.in/",
-  
-    distDir: "export/phenomenon",
+    // distDir: "export/phenomenon",
     typescript: {
       ignoreBuildErrors: true,
     },
