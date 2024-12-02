69fe1b52df2dec61a7ef994fba0c15c909e057d8
roshan2007git <roshan.pramod.2007@gmail.com>
2024-12-02 16:16:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //when testing through yarn dev, comment the whole thing till webpack
+    // // when testing through yarn dev, comment the whole thing till webpack
     // output: "export",
     // images: { unoptimized: true },
     // trailingSlash: true,
     // basePath: process.env.NODE_ENV === "production" ? "/phenomenon" : "",
   
-    // // // //uncomment this when not testing export
+    // // //uncomment this when not testing export
     // // assetPrefix:
     // //   process.env.NODE_ENV === "production"
-    // //     ? "http://127.0.0.1:5500/sjbhsmun/"
+    // //     ? "http://127.0.0.1:5500/phenomenon/"
     // //     : "http://127.0.0.1:5500/",
   
-    // // //uncomment this when exporting file for integro
+    // //uncomment this when exporting file for integro
     // assetPrefix:
     //   process.env.NODE_ENV === "production"
     //     ? "https://sjbhs.edu.in/phenomenon"
@@ -23,7 +23,7 @@ const nextConfig = {
       ignoreBuildErrors: true,
     },
     eslint: {
-      ignoreDuringBuilds: process.env.NEXT_PUBLIC_DISABLE_LINT === 'true',
+      ignoreDuringBuilds: true,
     },
   };
   
