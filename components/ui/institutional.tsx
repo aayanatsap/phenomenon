b73568b2b8965027bed4cfc0aebb0b5f8872cfd1
Aayan Bothra <110114427+aayanbothra@users.noreply.github.com>
2024-12-10 20:43:48 +0530

    Institutional Completed

--- components/ui/institutional.tsx
+++ components/ui/institutional.tsx
@@ -9,20 +9,20 @@ import logo from "../../media/logo.png";
 export function Institutional() {
   return (
     <CardContainer className="inter-var text-center">
-      <CardBody className="bg-purple-900 relative  dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl px-6 py-12 border text-center flex flex-col items-center">
+      <CardBody className="bg-lightblue relative  dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl px-6 py-12 border text-center flex flex-col items-center">
         <CardItem
           translateZ="50"
-          className="text-3xl font-bold text-neutral-600 dark:text-white mx-auto font-aquireBold"
+          className="text-3xl font-bold text-whitepink dark:text-white mx-auto font-aquireBold"
         >
           INSTITUTIONAL
         </CardItem>
         <CardItem
           translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
+          className="text-whitepink text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
         >
           <>
             <div className="flex flex-col justify-center items-center my-4">
-              <span className="text-4xl font-aquire">Rs.300 per person</span>
+              <span className="text-4xl font-aquire text-babyblue">Rs.300 per person</span>
             </div>
 
             <ul role="list" className="mb-10 space-y-2 text-center font-trench text-xl font-extrabold">
