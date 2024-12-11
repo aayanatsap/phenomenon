c33ec74eed36c008fc86b2114c7ed95ad3cffc3f
roshan2007git <roshan.pramod.2007@gmail.com>
2024-12-11 11:18:11 +0530

    Update page.tsx

--- app/registrations/page.tsx
+++ app/registrations/page.tsx
@@ -16,7 +16,8 @@ export default function page() {
     "General Passes for each day will be available for purchase on that day on campus.",  
     "Registration for an event includes entry to the campus, concert/artist performance access and event participation on the day of the event only. All events except Movie Business are one-day events only. For entry into the campus on the other day, a general pass will have to be purchased.",  
     "No refunds will be processed.",  
-    "Registration for multiple events is permitted. Please check the schedule to ensure that there are no clashes.",  
+    "Registration for multiple events is permitted. Please check the schedule to ensure that there are no clashes.",
+    "Teachers accompanying the participants are required to carry their school ID cards for varification purposes. "  
   ]
 
   return (
@@ -45,28 +46,28 @@ export default function page() {
             </Link>
           </div>
           <div className="hidden lg:block">
-            <div className=" lg:grid lg:grid-cols-2 lg:space-y-0  -space-y-28 -mt-16 -mb-20">
+            <div className=" lg:grid lg:grid-cols-2 lg:space-y-0  -space-y-28 ">
             <Individual/>
             <Institutional/>
             </div>
           </div>
-          <div className=" lg:hidden grid grid-flow-row justify-items-center gap-24 mb-14">
+          <div className=" lg:hidden grid grid-flow-row justify-items-center gap-24 mb-14 mt-20">
             <div className="inter-var text-center">
-              <div className="bg-purple-900 relative  dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl px-6 py-12 border text-center flex flex-col items-center">
+              <div className="bg-lightblue relative  dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl px-6 py-12 border text-center flex flex-col items-center">
                 <div
-                  className="text-3xl font-bold text-neutral-600 dark:text-white mx-auto"
+                  className="text-3xl font-bold text-whitepink mx-auto font-aquireBold"
                 >
                   INDIVIDUAL
                 </div>
                 <div
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
+                  className="text-whitepink text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
                 >
                   <div>
                     <div className="flex flex-col justify-center items-center my-4">
-                      <span className="text-5xl font-extrabold">₹300 per person</span>
+                      <span className="text-4xl text-babyblue font-aquire">₹300 per person</span>
                     </div>
 
-                    <ul role="list" className="mb-10 space-y-2 text-center">
+                    <ul role="list" className="mb-10 space-y-2 text-center font-trench font-bold text-xl">
                       <li className="text-center">
                         <span>Some events require more than one participant</span>
                       </li>
@@ -91,7 +92,7 @@ export default function page() {
                 <div  className="justify-center items-center mt-12 w-full inline-block">
                 <Link href={"/registrations/individual/"}>
                 <div
-                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold w-[80%] mx-auto"
+                    className="px-4 py-2 rounded-xl font-aquireBold bg-black dark:bg-white dark:text-black text-white text-xs font-bold w-[80%] mx-auto"
                   >
                     REGISTER NOW!
                   </div>
@@ -100,54 +101,54 @@ export default function page() {
               </div>
             </div>
             
-            {/* <div className="inter-var text-center">
-  <div className="bg-purple-900 relative  dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl px-6 py-12 border text-center flex flex-col items-center">
-    <div
-      className="text-3xl font-bold text-neutral-600 dark:text-white mx-auto"
-    >
-      Institutional
-    </div>
-    <div
-      className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
-    >
-      <div>
-        <div className="flex flex-col justify-center items-center my-4">
-          <span className="text-5xl font-extrabold">₹300 per person</span>
-        </div>
+            <div className="inter-var text-center">
+              <div className="bg-lightblue relative  dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl px-6 py-12 border text-center flex flex-col items-center">
+                <div
+                  className="text-3xl font-bold text-whitepink font-aquireBold dark:text-white mx-auto"
+                >
+                  INSTITUTIONAL
+                </div>
+                <div
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
+                >
+                  <div className='text-whitepink'>
+                    <div className="flex flex-col justify-center items-center my-4">
+                      <span className="text-4xl font-aquire text-babyblue">₹300 per person</span>
+                    </div>
 
-        <ul role="list" className="mb-10 space-y-2 text-center">
-          <li className="text-center">
-            <span>You must register for a miminum of 6 events</span>
-          </li>
-          <li className="text-center">
-            <span>10% discount will be given on the final pricing</span>
-          </li>
-          <li className="text-center">
-            <span>You can register for a maximum of one event per day</span>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div className="w-full mt-0 bg-opacity-0">
-      <Image
-        src={logo}
-        height="1000"
-        width="1000"
-        className="h-60 w-full object-contain rounded-xl "
-        alt="thumbnail"
-      />
-    </div>
-    <div className="justify-center items-center mt-12 w-full inline-block">
-    <Link href={"/registrations/delegation/"}>
-      <div
-        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold w-[80%] mx-auto"
-      >
-        REGISTER NOW!
-      </div>
-      </Link>
-    </div>
-  </div>
-</div> */}
+                    <ul role="list" className="mb-10 space-y-2 text-center font-trench text-xl font-bold">
+                      <li className="text-center">
+                        <span>You must register for a miminum of 6 events</span>
+                      </li>
+                      <li className="text-center">
+                        <span>10% discount will be given on the final pricing</span>
+                      </li>
+                      <li className="text-center">
+                        <span>You can register for a maximum of one event per day</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="w-full mt-0 bg-opacity-0">
+                  <Image
+                    src={logo}
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-contain rounded-xl "
+                    alt="thumbnail"
+                  />
+                </div>
+                <div className="justify-center items-center mt-12 w-full inline-block">
+                <Link href={"/registrations/delegation/"}>
+                  <div
+                    className="px-4 py-2 rounded-xl font-aquireBold bg-black dark:bg-white dark:text-black text-white text-xs font-bold w-[80%] mx-auto"
+                  >
+                    REGISTER NOW!
+                  </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
